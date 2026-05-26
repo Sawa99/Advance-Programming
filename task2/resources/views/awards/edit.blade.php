4a1165932297ae13d9a55897719f6536cd411a4a
Sarah Wild <117588659+Sawa99@users.noreply.github.com>
2026-05-26 10:54:19 +0100

    Delete task2 directory in Task-1 branch

--- task2/resources/views/awards/edit.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Edit {{$award->name }}</h2>
-
-<form>
-    @csrf
-    <label for="name">Name</label>
-    <input type="text" name="name" value="{{$award->name}}">
-
-    <input type="submit" value="Update">
-</form>
